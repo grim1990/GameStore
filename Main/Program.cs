e0e81ceb67e7cb8a387a461df26e0cdd11c5c11f
grim1990 <grim1990@gmail.com>
2022-01-15 20:37:59 +0100

    Zmiana

--- Main/Program.cs
+++ Main/Program.cs
@@ -87,7 +87,7 @@ namespace Game_Store
                                         GamesBase.BuyGame();
 
 
-                                        int input2 = Common.GetInt("Would you like to buy another game?", "Please select game ID numberz ");
+                                        int input2 = Common.GetInt("Would you like to buy another game?", "Please select game ID number");
 
                                         if (input2 == 0)
                                         {
